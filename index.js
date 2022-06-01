1cc90a4269427adb0614deedfe1b9de60a521ed9
SugaWrld <91806260+SugaWrld@users.noreply.github.com>
2022-06-01 16:20:36 -0400

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-let lang = document.getElementById("language").value;
-let text = document.getElementById("welcome");
-
-function language(){
-    if (lang === 1){
-        text.innerText="Hello";
-    }
-}
